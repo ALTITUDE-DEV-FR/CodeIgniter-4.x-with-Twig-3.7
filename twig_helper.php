1226f68b471280243c99450eb7f1aa5931fc56df
ALTITUDE DEV <contact@altitude-dev.fr>
2023-12-26 12:49:03 +0100

    Update twig_helper.php

--- twig_helper.php
+++ twig_helper.php
@@ -1,47 +1,61 @@
-<?php 
-
+<?php
 /*
 * TWIG CI ANDROMEDE CMS
 */
 use Twig\Environment;
 use Twig\Extra\Cache\CacheExtension;
 use Twig\Extra\CssInliner\CssInlinerExtension;
-
 use Symfony\Component\Cache\Adapter\FilesystemAdapter;
 use Symfony\Component\Cache\Adapter\TagAwareAdapter;
 use Twig\Extra\Cache\CacheRuntime;
 use Twig\RuntimeLoader\RuntimeLoaderInterface;
+use App\ThirdParty\TwigExtensions\JsonDecodeExtension;
 
 if (! function_exists('twig')) {
-    function twig($cache, $optimized){
+    function twig($cache, $optimized, $pathOveride){
 
-        $loader = new \Twig\Loader\FilesystemLoader(APPPATH.'/Views/front/themes/'.THEME.'/twig');
+        $loader = new \Twig\Loader\FilesystemLoader(APPPATH);
         $twig   = new \Twig\Environment($loader, [
             'cache'             => $cache ? WRITEPATH.'/cache/twig' : false,
             'auto_reload'       => true,
             'charset'           => 'utf-8',
             'optimizations'     => $optimized ? -1 : 0,
-            'strict_variables'  => true,
+            'strict_variables'  => false,
             'debug'             => false
         ]);
-
+   
         $twig->addExtension(new CssInlinerExtension());
         $twig->addExtension(new \Twig\Extension\DebugExtension());
         $twig->addExtension(new CacheExtension());
+        $twig->addExtension(new JsonDecodeExtension());
 
         $twig->addRuntimeLoader(new class implements RuntimeLoaderInterface {
             public function load($class) {
                 if (CacheRuntime::class === $class) {
-                    return new CacheRuntime(new TagAwareAdapter(new FilesystemAdapter()));
+                    return new CacheRuntime(new TagAwareAdapter(new FilesystemAdapter('andromede_cached', 0, WRITEPATH . '/cache/twig')));
                 }
             }
         });
-    
-        // CONTSTANT
-        $twig->addGlobal('THEME', THEME); //Exemple to add SuperGlobal CI to Twig
-  
-        // CI FUNCTIONS TO TWIG
+
+        /*
+        * Global Defines CI > Twig
+        */
+        $twig->addGlobal('TTLCONTENT', CACHE_TIME);
+        $twig->addGlobal('THEME', THEME);
+        $twig->addGlobal('RGPD_NOTICE', RGPD_NOTICE);
+        $twig->addGlobal('MATOMO_ID', MATOMO_ID);
+        $twig->addGlobal('LANGAVAILABLE', LANGAVAILABLE);
+        $twig->addGlobal('LANGSELECTOR', LANGSELECTOR);
+        $twig->addGlobal('SINGLENEWS_PATH', SINGLENEWS_PATH);
+
+        /*
+        * Functions CI > Twig
+        */
+        $twig->addFunction(new \Twig\TwigFunction('lang', 'lang'));
+        $twig->addFunction(new \Twig\TwigFunction('session', 'session'));
+        $twig->addFunction(new \Twig\TwigFunction('url_to', 'url_to'));
         $twig->addFunction(new \Twig\TwigFunction('csrf_hash', 'csrf_hash'));
+        $twig->addFunction(new \Twig\TwigFunction('csrf_token', 'csrf_token'));
         $twig->addFunction(new \Twig\TwigFunction('site_url', 'site_url'));
         $twig->addFunction(new \Twig\TwigFunction('base_url', 'base_url'));
         $twig->addFunction(new \Twig\TwigFunction('current_url', 'current_url'));
